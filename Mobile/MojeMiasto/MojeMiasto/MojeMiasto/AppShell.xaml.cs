3ffe0f9106a12a9134d8a79c4dc9fc5d752324f5
EFox <elementalsfox@gmail.com>
2022-10-08 23:48:03 +0200

    Xamarin clear project

---
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MojeMiasto.ViewModels;
+using MojeMiasto.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MojeMiasto
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
