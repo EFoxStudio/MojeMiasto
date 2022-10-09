4cae0d2c584906702dd6e6e6faf0cfe679493526
EFox <elementalsfox@gmail.com>
2022-10-09 12:12:50 +0200

    clear api project
    include api key

---
+++ Api/MojeMiasto/MojeMiasto/Filters/ApiKeyAuth.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MojeMiasto.Filters
+{
+
+    public class ApiKeyAuth
+    {
+        private readonly RequestDelegate _next;
+        private
+        const string APIKEY = "ApiKey";
+        public ApiKeyAuth(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //skip key if accesing icons
+            string url = context.Request.Path;
+            if (url.Contains("icons"))
+                await _next(context);
+
+
+            if (!context.Request.Headers.TryGetValue(APIKEY, out
+                    var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Api Key was not provided ");
+                return;
+            }
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = appSettings.GetValue<string>(APIKEY);
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client");
+                return;
+            }
+            await _next(context);
+        }
+    }
+}
