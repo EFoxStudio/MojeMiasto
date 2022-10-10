c7da095379f2d28df72ec5695c76ed8d57538913
Yo FIlip <102390337+YoFilip@users.noreply.github.com>
2022-10-10 22:12:27 +0200

    Dodanie komentarza
    Dodałem mały komentarz ,,to do"

--- Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Views/HomePage.xaml.cs
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Views/HomePage.xaml.cs
@@ -15,6 +15,15 @@ namespace MojeMiasto.Views
         public HomePage()
         {
             InitializeComponent();
+
+            //Janek to do list:
+            /*
+             * Zapoznać się z strukurą i siąść do tego do środy do niedzieli albo tego nie zrobimy!!!!
+             * Stworzyć zakładkę informacyja do eventu +
+             * Dodać tablice z miastami. +
+             * Stworzyć jeden przykłądowy box i do niego stworzyć tablicę z Miejscem i potem dopisywać miejsca +
+             * Do boxa 2 dodać datę zaciągniętą z bazy danych (Event/date)
+             */
         }
     }
 }
