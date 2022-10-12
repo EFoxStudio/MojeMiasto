fc7ef6af19e2ca2b6a8dc6fabdfdac9f17aa85cc
EFox <elementalsfox@gmail.com>
2022-10-12 23:31:39 +0200

    Login base

---
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/ViewModels/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MojeMiasto.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MojeMiasto.ViewModels
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+
+        [RelayCommand]
+        public void OpenMenu()
+        {
+            Shell.Current.FlyoutIsPresented = true;
+
+        }
+
+        [RelayCommand]
+        public async void ToLoginPage()
+        {
+            await Shell.Current.GoToAsync(nameof(LoginPage));
+        }
+
+    }
+}
--- Mobile/MojeMiasto/MojeMiasto/MojeMiasto/ViewModels/LoginViewModel.cs
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/ViewModels/LoginViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using EFox.ApiConnection.Toolkit;
+using MojeMiasto.Models;
 using MojeMiasto.Views;
 using System;
 using System.Collections.Generic;
@@ -8,13 +10,32 @@ using Xamarin.Forms;
 
 namespace MojeMiasto.ViewModels
 {
-    internal partial class LoginViewModel : ObservableObject
+    internal partial class LoginViewModel : BaseViewModel
     {
+        Connection<User> conn = new Connection<User>("https://api.efox.com.pl/mycity/");
+
+
+        [ObservableProperty]
+        string email;
+
+        [ObservableProperty]
+        string password;
+
+
         [RelayCommand]
-        public void OpenMenu()
+        public async void Login()
         {
-            Shell.Current.FlyoutIsPresented = true;
+            User user = await conn.Get($"users/email/{ Email }");
+
+            if (user == default(User))
+                return;
+
+            if (user.password == Password)
+            {
+                //ToHomePage();
+            }
 
         }
+
     }
 }
