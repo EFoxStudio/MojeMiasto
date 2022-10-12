88ce78ca4421f6999910c64a185ec3c87b306f46
EFox <elementalsfox@gmail.com>
2022-10-12 18:14:48 +0200

    .

--- Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Views/HomePage.xaml.cs
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Views/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MojeMiasto.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace MojeMiasto.Views
         public HomePage()
         {
             InitializeComponent();
+            BindingContext = new HomeViewModel();
         }
     }
 }
