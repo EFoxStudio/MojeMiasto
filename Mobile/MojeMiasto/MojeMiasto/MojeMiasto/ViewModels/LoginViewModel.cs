f84c896ae9759ce166516b525b5a6351dc93572d
Yo FIlip <102390337+YoFilip@users.noreply.github.com>
2022-10-12 23:13:24 +0200

    Add LoginPage

--- Mobile/MojeMiasto/MojeMiasto/MojeMiasto/ViewModels/LoginViewModel.cs
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/ViewModels/LoginViewModel.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MojeMiasto.Views;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace MojeMiasto.ViewModels
 {
-    internal class LoginViewModel
+    internal partial class LoginViewModel : ObservableObject
     {
+        [RelayCommand]
+        public void OpenMenu()
+        {
+            Shell.Current.FlyoutIsPresented = true;
+
+        }
     }
 }
