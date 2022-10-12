6ef1e20a6fbd3014dd4e172d8f5d173139e4b0d0
EFox <elementalsfox@gmail.com>
2022-10-12 23:09:58 +0200

    add user data to class

--- Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Models/User.cs
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Models/User.cs
@@ -7,5 +7,12 @@ namespace MojeMiasto.Models
     
     internal class User
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int city_id { get; set; }
+        public int region_id { get; set; }
+        public int points { get; set; }
     }
 }
