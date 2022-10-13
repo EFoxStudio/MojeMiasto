363e5ec0440c92ea6bed77d18efe13ac07af9443
EFox <elementalsfox@gmail.com>
2022-10-13 20:00:24 +0200

    Cities search

---
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Models/Chat.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MojeMiasto.Models
+{
+    public class Chat
+    {
+        [Key]
+        public int id { get; set; }
+        public int user1_id { get; set; }
+        public int user2_id { get; set; }
+    }
+}
---
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/ViewModels/LocationViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EFox.ApiConnection.Toolkit;
+using MojeMiasto.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MojeMiasto.ViewModels
+{
+    internal partial class LocationViewModel : BaseViewModel
+    {
+        Connection<City> conn = new Connection<City>("https://api.efox.com.pl/mycity/");
+
+        [ObservableProperty]
+        string city;
+        [ObservableProperty]
+        string district;
+
+
+        [ObservableProperty]
+        ObservableCollection<City> cities;
+
+
+        public LocationViewModel()
+        {
+            conn.AddHeader("ApiKey", "g84@RRGA%!bP8vNzK7p&uLXz&");
+        }
+
+
+
+        [RelayCommand]
+        public async void SearchCities()
+        {
+            List<City> citiesList = await conn.GetList($"cities/search/{ City }");
+            if(citiesList == null)
+                return;
+            Cities = new ObservableCollection<City>(citiesList);
+        }
+    }
+}
