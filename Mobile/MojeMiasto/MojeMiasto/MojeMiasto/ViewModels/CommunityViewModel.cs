cbcd08e75b115f34d7b52db4b12428e1fe650c4f
zcmbv <97365964+zcmbv@users.noreply.github.com>
2022-10-13 19:55:55 +0200

    Created commnity page

---
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/ViewModels/CommunityViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EFox.ApiConnection.Toolkit;
+using MojeMiasto.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MojeMiasto.ViewModels
+{
+    internal partial class CommunityViewModel : BaseViewModel
+    {
+        //Variable to get info from database
+        Connection<User> con = new Connection<User>("https://api.efox.com.pl/mycity/");
+
+        public CommunityViewModel()
+        {
+            con.AddHeader("ApiKey", "g84@RRGA%!bP8vNzK7p&uLXz&");
+        }
+        //List to store users
+        [ObservableProperty]
+        ObservableCollection<User> usersList = new ObservableCollection<User>();
+
+        [ObservableProperty]
+        string cityNr;
+
+        [RelayCommand]
+        public async void AddToCollection()
+        {
+            List<User> users = await con.GetList($"users/city_id/{CityNr}");
+
+            foreach (User cur in users)
+            {
+                UsersList.Add(cur);
+            }
+
+        }
+
+    }
+}
---
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Views/CommunityPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MojeMiasto.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CommunityPage : ContentPage
+    {
+        public CommunityPage()
+        {
+            InitializeComponent();
+            BindingContext = new CommunityPage();
+        }
+    }
+}
