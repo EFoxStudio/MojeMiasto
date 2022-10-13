8bdf25e88ebce06c133c1f85c5cdcec3ea95608b
zcmbv <97365964+zcmbv@users.noreply.github.com>
2022-10-13 20:15:58 +0200

    .

--- Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Views/CommunityPage.xaml.cs
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Views/CommunityPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MojeMiasto.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,7 @@ namespace MojeMiasto.Views
         public CommunityPage()
         {
             InitializeComponent();
-            BindingContext = new CommunityPage();
+            BindingContext = new CommunityViewModel();
         }
     }
 }
