ff3b3efd2ab373410d2e2d49a22c3c17d3516da0
EFox <elementalsfox@gmail.com>
2022-10-13 20:11:02 +0200

    added classes

---
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Models/District.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MojeMiasto.Models
+{
+    public class District
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public int city_id { get; set; }
+    }
+}
---
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Models/Quest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MojeMiasto.Models
+{
+    public class Quest
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string reward { get; set; }
+        public int user_id { get; set; }
+        public int city_id { get; set; }
+        public int district_id { get; set; }
+        public DateTime create_date { get; set; }
+        public DateTime end_date { get; set; }
+        public int hired_id { get; set; }
+    }
+}
