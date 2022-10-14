08acb934d33a76bcd588897451bfcf545955cb13
zcmbv <97365964+zcmbv@users.noreply.github.com>
2022-10-14 20:29:04 +0200

    Added page and view model

---
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/ViewModels/ChatViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EFox.ApiConnection.Toolkit;
+using MojeMiasto.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MojeMiasto.ViewModels
+{
+    internal partial class ChatViewModel
+    {
+        Connection<Chat> chat_conn = new Connection<Chat>("https://api.efox.com.pl/mycity/");
+    }
+}
