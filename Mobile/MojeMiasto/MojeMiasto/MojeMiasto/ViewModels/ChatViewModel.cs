9a974d6836e9128a39bea9c98f6da22d1e64ca86
zcmbv <97365964+zcmbv@users.noreply.github.com>
2022-10-15 11:27:57 +0200

    .

--- Mobile/MojeMiasto/MojeMiasto/MojeMiasto/ViewModels/ChatViewModel.cs
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/ViewModels/ChatViewModel.cs
@@ -13,21 +13,25 @@ namespace MojeMiasto.ViewModels
 {
     internal partial class ChatViewModel : BaseViewModel
     {
-        Connection<Chat> user_conn = new Connection<Chat>("https://api.efox.com.pl/mycity/");
         Connection<Chat> chat_conn = new Connection<Chat>("https://api.efox.com.pl/mycity/");
 
         [ObservableProperty]
-        ObservableCollection<Chat> chats = new ObservableCollection<Chat>();
+        ObservableCollection<Chat> chats;
 
         public ChatViewModel()
         {
-            user_conn.AddHeader("ApiKey", "g84@RRGA%!bP8vNzK7p&uLXz&");
             chat_conn.AddHeader("ApiKey", "g84@RRGA%!bP8vNzK7p&uLXz&");
         }
 
+        [RelayCommand]
         public async void ShowChat()
         {
-
+            
+            int user_id = Preferences.Get("user_id", 0);
+            if (user_id == null)
+                return;
+            List<Chat> chat = await chat_conn.GetList($"chat/user_id/{user_id}");
+            Chats = new ObservableCollection<Chat>(chat);
         }
 
     }
