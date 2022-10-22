95f0789aee2c1ff5d302d7f3557c25274d11e8fd
EFox <elementalsfox@gmail.com>
2022-10-22 19:06:13 +0200

    .

--- Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Models/UI_Quest.cs
+++ Mobile/MojeMiasto/MojeMiasto/MojeMiasto/Models/UI_Quest.cs
@@ -11,7 +11,7 @@ namespace MojeMiasto.Models
         public string name { get; set; }
         public string description { get; set; }
         public int user_id { get; set; }
-        public User user { get; set; }
+        public UI_User user { get; set; }
         public int city_id { get; set; }
         public string city { get; set; }
         public int district_id { get; set; }
@@ -20,7 +20,7 @@ namespace MojeMiasto.Models
         public DateTime end_date { get; set; }
         public bool isHired { get; set; }
         public int hired_id { get; set; }
-        public User hired { get; set; }
+        public UI_User hired { get; set; }
         public bool done { get; set; }
         public string location { get; set; }
     }
